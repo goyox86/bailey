939442bc3e0d0185f5cd7d825823b23018c1d213
Jose Narvaez <jnarvaez@zendesk.com>
2016-02-14 23:57:52 +0000

    Very basic CLI.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@
 extern crate oak_runtime;
 use oak_runtime::*;
 
+extern crate getopts;
+use getopts::Options;
+use std::env;
+use std::path::Path;
+
 use std::io::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
@@ -303,10 +308,44 @@ grammar! bailey {
     }
 }
 
+fn print_usage(program: String, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    let f = File::open("./src/example.bly").ok().expect("failed to open file");
-    let mut reader = BufReader::new(f);
-    let mut contents = String::new();
-    reader.read_to_string(&mut contents);
-    println!("{:#?}", bailey::parse_program(contents.stream()).unwrap_data());
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("e", "", "evaluate expression", "EXP");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(program, opts);
+        return;
+    }
+
+    let file_path = args[1].clone();
+    if Path::new(&file_path).exists() {
+        let f = File::open(file_path).ok().expect("failed to open file");
+        let mut reader = BufReader::new(f);
+        let mut code = String::new();
+        reader.read_to_string(&mut code);
+        println!("{:#?}", bailey::parse_program(code.stream()).unwrap_data());
+        return
+    }
+
+    let expression = matches.opt_str("e");
+    match expression {
+        Some(e) => {
+            println!("{:#?}", bailey::parse_program(e.stream()).unwrap_data());
+            return
+        },
+        None => print_usage(program, opts)
+    };
 }
