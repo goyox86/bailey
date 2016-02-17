db651af15fbf4c783dec2cfad7287a2693a0556e
Jose Narvaez <goyox86@gmail.com>
2016-02-17 23:56:12 +0000

    Using structs for the values os the AST nodes. Not sure if this is better or not.

--- src/ast.rs
+++ src/ast.rs
@@ -1,19 +1,19 @@
+
 #[derive(Debug, Clone)]
 pub enum Node {
-    Identifier(String),
-    Constant(String),
-    IntegerLiteral(u32),
-    FloatLiteral(f32),
-    StringLiteral(String),
-    BinaryExpr(BinOp, PNode, PNode),
-    AssingExpr(PNode, PNode),
-    ClassDecl(PNode, Vec<PNode>),
-    IfStatement(PNode, Vec<PNode>, Option<Vec<PNode>>),
-    MethodDecl(PNode, Option<Vec<PNode>>, Vec<PNode>),
-    WhileStatement(PNode, Vec<PNode>),
-    Message(Option<PNode>, PNode, Option<Vec<PNode>>),
-    Block(Vec<PNode>)
-
+    Identifier { name: String },
+    Constant { name: String },
+    IntegerLiteral { value: u32 },
+    FloatLiteral { value: f32 },
+    StringLiteral { value: String },
+    BinaryExpression { op: BinOp, left: PNode, right: PNode },
+    AssingExpression { identifier: PNode, expression: PNode },
+    ClassDeclaration { identifier: PNode, methods: Vec<PNode> },
+    MethodDeclaration { method: PNode, params: Option<Vec<PNode>>, block: PNode },
+    IfStatement { condition: PNode, true_block: PNode, false_block: Option<PNode> },
+    WhileStatement { condition: PNode, block: PNode },
+    Message { receiver: Option<PNode>, method: PNode, args: Option<Vec<PNode>> },
+    Block { instructions: Vec<PNode> }
 }
 
 #[derive(Debug, Clone)]
