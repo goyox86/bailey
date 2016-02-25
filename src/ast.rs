8911dbff9689218e1de702be4a6cfd88d3f2c888
Jose Narvaez <jnarvaez@zendesk.com>
2016-02-25 14:15:54 +0000

    Rustfmt on AST.

--- src/ast.rs
+++ src/ast.rs
@@ -2,91 +2,102 @@
 pub struct Message {
     pub receiver: Option<PNode>,
     pub method: PNode,
-    pub args: Vec<PNode>
+    pub args: Vec<PNode>,
 }
 
 #[derive(Debug, Clone)]
 pub struct Block {
-    pub instructions: Vec<PNode>
+    pub instructions: Vec<PNode>,
 }
 
 #[derive(Debug, Clone)]
 pub struct Class {
     pub identifier: PNode,
-    pub methods: Vec<PNode>
+    pub methods: Vec<PNode>,
 }
 
 #[derive(Debug, Clone)]
 pub struct Method {
     pub method: PNode,
     pub params: Vec<PNode>,
-    pub block: PNode
+    pub block: PNode,
 }
 
 #[derive(Debug, Clone)]
 pub struct If {
     pub condition: PNode,
     pub true_block: PNode,
-    pub false_block: Option<PNode>
+    pub false_block: Option<PNode>,
 }
 
 #[derive(Debug, Clone)]
 pub struct While {
     pub condition: PNode,
-    pub block: PNode
+    pub block: PNode,
 }
 
 #[derive(Debug, Clone)]
 pub struct Assignment {
     pub identifier: PNode,
-    pub expression: PNode
+    pub expression: PNode,
 }
 
 #[derive(Debug, Clone)]
 pub struct Identifier {
-    pub name: String
+    pub name: String,
 }
 
 #[derive(Debug, Clone)]
 pub struct Constant {
-    pub name: String
+    pub name: String,
 }
 
 #[derive(Debug, Clone)]
 pub struct IntegerLiteral {
-    pub value: u32
+    pub value: u32,
 }
 
 #[derive(Debug, Clone)]
 pub struct FloatLiteral {
-    pub value: f32
+    pub value: f32,
 }
 
 #[derive(Debug, Clone)]
 pub struct StringLiteral {
-    pub value: String
+    pub value: String,
 }
 
 #[derive(Debug, Clone)]
 pub struct ArrayLiteral {
-    pub elements: Vec<PNode>
+    pub elements: Vec<PNode>,
 }
 
 #[derive(Debug, Clone)]
 pub struct MapLiteral {
-    pub entries: Vec<(PNode, PNode)>
+    pub entries: Vec<(PNode, PNode)>,
 }
 
 #[derive(Debug, Clone)]
 pub struct BinaryExpression {
     pub op: BinOp,
     pub left: PNode,
-    pub right: PNode
+    pub right: PNode,
 }
 
 #[derive(Debug, Clone)]
 pub enum BinOp {
-    Add, Sub, Mul, Div, Lt, Lte, Gt, Gte, Ne, Eq, And, Or
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Lt,
+    Lte,
+    Gt,
+    Gte,
+    Ne,
+    Eq,
+    And,
+    Or,
 }
 
 #[derive(Debug, Clone)]
@@ -105,7 +116,7 @@ pub enum Node {
     BlockExpr(Block),
     MessageExpr(Message),
     ClassDecl(Class),
-    MethodDecl(Method)
+    MethodDecl(Method),
 }
 
 pub type PNode = Box<Node>;
